601cdd15994884ca88b8674d1d8d03279006fd6f
EliasDiego <50988935+EliasDiego@users.noreply.github.com>
2021-05-10 12:53:13 +0800

    Transferred Menu Script (Though needs some reworking) Added SimpleSingleton Script Added Empty ObjectPool

---
+++ Assets/NeedsVsWants/Scripts/Patterns/ObjectPool.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NeedsVsWants.Patterns
+{
+    public class ObjectPool : SimpleSingleton<ObjectPool>
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+}
