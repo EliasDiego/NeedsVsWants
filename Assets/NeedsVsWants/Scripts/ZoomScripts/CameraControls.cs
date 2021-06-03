2c48c9bc1f6e9a11da23e6aedb6ef2845f35eb9a
Zoomboy <79925805+Zoom397@users.noreply.github.com>
2021-06-03 18:19:25 +0800

    Added Mayo's 3D assets to DaleScene. Added Camera controls for checking.

---
+++ Assets/NeedsVsWants/Scripts/ZoomScripts/CameraControls.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace NeedsVsWants.CameraSystem
+{
+
+
+
+    public class CameraControls : MonoBehaviour
+    {
+        [SerializeField] InputActionReference camerabindings;
+        [SerializeField] float panSpeed = 20f;
+
+        void Start()
+        {
+            camerabindings.action.performed += StartCameraMovement; 
+        }
+
+        void StartCameraMovement (InputAction.CallbackContext context)
+        {
+            Debug.Log(context.ReadValue <Vector2>());
+
+        }
+        // Update is called once per frame
+        void Update()
+        {
+           /* Vector3 pos = transform.position;
+
+            if (Input.GetKey("w"))
+            {
+                pos.z += panSpeed * Time.deltaTime;
+            }
+
+            if (Input.GetKey("s"))
+            {
+                pos.z -= panSpeed * Time.deltaTime;
+            }
+
+            if (Input.GetKey("d"))
+            {
+                pos.z += panSpeed * Time.deltaTime;
+            }
+
+            if (Input.GetKey("a"))
+            {
+                pos.z -= panSpeed * Time.deltaTime;
+            }
+
+            transform.position = pos;*/
+        }
+    }
+}
