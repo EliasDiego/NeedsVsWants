3d5f5c090311a71ebb59cfd180a9a2d0f56c4dff
EliasDiego <50988935+EliasDiego@users.noreply.github.com>
2021-06-11 10:15:54 +0800

    Quick Commit

--- Assets/NeedsVsWants/Scripts/MessagingSystem/ChatViewerMenu.cs
+++ Assets/NeedsVsWants/Scripts/MessagingSystem/ChatViewerMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
+using UnityEngine.UI;
 
 using NeedsVsWants.Patterns;
 using NeedsVsWants.MenuSystem;
--- Assets/NeedsVsWants/Scripts/MessagingSystem/MessageBox.cs
+++ Assets/NeedsVsWants/Scripts/MessagingSystem/MessageBox.cs
@@ -16,12 +16,28 @@ namespace NeedsVsWants.MessagingSystem
         TMP_Text _Text;
         [SerializeField]
         Image _ProfilePicture;
+        [SerializeField]
+        LayoutElement _BoxLayout;
+        [SerializeField]
+        float _CharacterLimit;
 
         HorizontalLayoutGroup _LayoutGroup;
+
+        int _CharacterCount = 0;
 
         void Awake() 
         {
             _LayoutGroup = GetComponent<HorizontalLayoutGroup>();    
+        }
+
+        void Update() 
+        {
+            if(_CharacterCount != _Sender.text.Length || _CharacterCount != _Text.text.Length)
+            {
+                _CharacterCount = _Sender.text.Length > _Text.text.Length ? _Sender.text.Length : _Text.text.Length;
+                
+                _BoxLayout.enabled = _CharacterCount > _CharacterLimit;
+            }
         }
         
         public void AssignMessage(Character sender, string text, bool isAnne)
