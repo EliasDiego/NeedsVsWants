8acc86d7996f3e636fd13d9790968b2d62d9aa9e
Zoomboy <79925805+Zoom397@users.noreply.github.com>
2021-07-07 07:48:47 +0800

    Added a new base script for State Machine's values. Serialized patrol state values.

--- Assets/NeedsVsWants/Scripts/MovementSystem/Patrol.cs
+++ Assets/NeedsVsWants/Scripts/MovementSystem/Patrol.cs
@@ -2,48 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Patrol : StateMachineBehaviour
+
+namespace NeedsVsWants.MovementSystem
 {
-
-    GameObject Ana;
-    GameObject[] waypoints;
-    int currentWP;
-
-    void Awake()
+    public class Patrol : AnaBaseFSM
     {
-        waypoints = GameObject.FindGameObjectsWithTag("waypoint");
-    }
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        Ana = animator.gameObject;
-        currentWP = 0;
-    }
 
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        if (waypoints.Length == 0) return;
-        if ( Vector3.Distance (waypoints[currentWP].transform.position, Ana.transform.position) < 3.0f)
+      
+        GameObject[] waypoints;
+        int currentWP;
+
+        void Awake()
         {
-            currentWP++;
-            if(currentWP >= waypoints.Length)
-            {
-                currentWP = 0;
-            }
+            waypoints = GameObject.FindGameObjectsWithTag("waypoint");
+        }
+        // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+        override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            base.OnStateEnter(animator, stateInfo, layerIndex);
+            currentWP = 0;
         }
 
-        //rotate towards target
-        var direction = waypoints[currentWP].transform.position - Ana.transform.position;
-        Ana.transform.rotation = Quaternion.Slerp(Ana.transform.rotation, Quaternion.LookRotation(direction), 1.0f * Time.deltaTime);
-        Ana.transform.Translate(0, 0, Time.deltaTime * 2.0f);
-    } 
+        // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+        override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (waypoints.Length == 0) return;
+            if (Vector3.Distance(waypoints[currentWP].transform.position, Ana.transform.position) < accuracy)
+            {
+                currentWP++;
+                if (currentWP >= waypoints.Length)
+                {
+                    currentWP = 0;
+                }
+            }
 
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-       
+            //rotate towards target
+            var direction = waypoints[currentWP].transform.position - Ana.transform.position;
+            Ana.transform.rotation = Quaternion.Slerp(Ana.transform.rotation, Quaternion.LookRotation(direction), rotspeed * Time.deltaTime);
+            Ana.transform.Translate(0, 0, Time.deltaTime * speed);
+        }
+
+        // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+        override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+
+        }
+
+
     }
-
-    
 }
