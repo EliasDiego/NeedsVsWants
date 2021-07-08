24b9f568e8a78cf37874362cec95317c0fe4fce7
Zoomboy <79925805+Zoom397@users.noreply.github.com>
2021-07-08 23:06:55 +0800

    Finished Ana movement.

--- Assets/NeedsVsWants/Scripts/MovementSystem/Messaging.cs
+++ Assets/NeedsVsWants/Scripts/MovementSystem/Messaging.cs
@@ -4,26 +4,27 @@ using UnityEngine;
 
 namespace NeedsVsWants.MovementSystem
 {
-    public class Busy : AnaBaseFSM
+    public class Messaging : AnaBaseFSM
     {
-        GameObject[] busypoint; //gets an error when not called as an array
+        GameObject[] msgpoint; //gets an error when not called as an array
         //Transform TeleportGoal;
 
         void Awake()
         {
-            busypoint = GameObject.FindGameObjectsWithTag("busypoint");
+            msgpoint = GameObject.FindGameObjectsWithTag("msgpoint");
         }
 
         override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
             base.OnStateEnter(animator, stateInfo, layerIndex);
-            
+
         }
 
         override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
             //TeleportGoal.position = busypoint[0].transform.position;
-            Ana.transform.position = busypoint[0].transform.position;
+            Ana.transform.position = msgpoint[0].transform.position;
+            Ana.transform.rotation = msgpoint[0].transform.rotation;
         }
 
         override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
--- Assets/NeedsVsWants/Scripts/MovementSystem/Shopping.cs
+++ Assets/NeedsVsWants/Scripts/MovementSystem/Shopping.cs
@@ -4,26 +4,27 @@ using UnityEngine;
 
 namespace NeedsVsWants.MovementSystem
 {
-    public class Busy : AnaBaseFSM
+    public class Shopping : AnaBaseFSM
     {
-        GameObject[] busypoint; //gets an error when not called as an array
+        GameObject[] shoppoint; //gets an error when not called as an array
         //Transform TeleportGoal;
 
         void Awake()
         {
-            busypoint = GameObject.FindGameObjectsWithTag("busypoint");
+            shoppoint = GameObject.FindGameObjectsWithTag("shoppoint");
         }
 
         override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
             base.OnStateEnter(animator, stateInfo, layerIndex);
-            
+
         }
 
         override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
             //TeleportGoal.position = busypoint[0].transform.position;
-            Ana.transform.position = busypoint[0].transform.position;
+            Ana.transform.position = shoppoint[0].transform.position;
+            Ana.transform.rotation = shoppoint[0].transform.rotation;
         }
 
         override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
