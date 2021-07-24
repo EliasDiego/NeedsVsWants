6918720172daa1dea50a2481b01c8dd9a4f07e28
Zoomboy <79925805+Zoom397@users.noreply.github.com>
2021-07-24 18:27:26 +0800

    Seethrough walls now working! Used different algorithm. Just need to fix trigger exit.

--- Assets/NeedsVsWants/Scripts/CameraSystem/WallHouse.cs
+++ Assets/NeedsVsWants/Scripts/CameraSystem/WallHouse.cs
@@ -22,9 +22,9 @@ namespace NeedsVsWants.CameraSystem
 
 
 
+            collider.gameObject.SetActive(false);
 
-
-                Debug.Log("Hits!");
+                //Debug.Log("Hits!");
                // SetMaterialTransparent();
 
                 //iTween.FadeTo(Roof, 0, 1);
@@ -33,9 +33,12 @@ namespace NeedsVsWants.CameraSystem
 
         }
 
-        private void Update()
+        void OnTriggerExit(Collider collider)
+
         {
-            
+
+            collider.gameObject.SetActive(true);
+
         }
 
         private bool IsCharacter(Collider collider)
@@ -49,85 +52,11 @@ namespace NeedsVsWants.CameraSystem
 
 
 
-        private void SetMaterialTransparent()
-
-        {
-
-            foreach (Material m in Roof.GetComponent<Renderer>().materials)
-
-            {
-
-                m.SetFloat("_Mode", 2);
-
-                m.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
-
-                m.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-
-                m.SetInt("_ZWrite", 0);
-
-                m.DisableKeyword("_ALPHATEST_ON");
-
-                m.EnableKeyword("_ALPHABLEND_ON");
-
-                m.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-
-                m.renderQueue = 3000;
-
-            }
-
-            
-
-        }
+     
 
 
 
-        private void SetMaterialOpaque()
-
-        {
-
-            foreach (Material m in Roof.GetComponent<Renderer>().materials)
-
-            {
-
-                m.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-
-                m.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
-
-                m.SetInt("_ZWrite", 1);
-
-                m.DisableKeyword("_ALPHATEST_ON");
-
-                m.DisableKeyword("_ALPHABLEND_ON");
-
-                m.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-
-                m.renderQueue = -1;
-
-            }
-
-        }
-
-
-
-        void OnTriggerExit(Collider collider)
-
-        {
-
-            if (IsCharacter(collider))
-
-            {
-
-                // Set material to opaque
-
-                iTween.FadeTo(Roof, 1, 1);
-
-
-
-                Invoke("SetMaterialOpaque", 1.0f);
-
-            }
-
-        }
+         
 
     }
 }
